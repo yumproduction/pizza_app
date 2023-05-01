33ecaad6e17a97ae8128ba1e88c355c74928031d
mekhribonu_s <mekhribonu.s@codexoptimus.pro>
2023-05-01 12:52:01 +0500

    first commit

---
+++ app/src/main/java/com/example/jetpackcomposedemo/data/FlowersData.kt
@@ -0,0 +1,24 @@
+package com.example.jetpackcomposedemo.data
+
+import com.example.jetpackcomposedemo.R
+
+object FlowersData{
+    val list = listOf(
+        Flowers(name = "Margaritta",
+            price = "$570.00",
+            image = R.drawable.ic_red_rose_bouquet
+        ),
+        Flowers(name = "Go'sth",
+            price = "$650.00",
+            image = R.drawable.ic_pink_rose_bouquet
+        ),
+        Flowers(name = "BBQ",
+            price = "$570.00",
+            image = R.drawable.ic_red_rose_bouquet
+        ),
+        Flowers(name = "Ranch",
+            price = "$650.00",
+            image = R.drawable.ic_pink_rose_bouquet
+        )
+    )
+}
